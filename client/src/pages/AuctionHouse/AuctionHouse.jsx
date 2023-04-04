36a692bfaf9987ad5947897b44f12616b307f52e
AlexScotte <scottealexandre@gmail.com>
2023-04-04 22:24:40 +0200

    [Marketplace] Add data grid to list items

--- client/src/pages/AuctionHouse/AuctionHouse.jsx
+++ client/src/pages/AuctionHouse/AuctionHouse.jsx
@@ -1,28 +1,93 @@
 import { DataGrid } from '@mui/x-data-grid';
-// import { createStyles, makeStyles } from "@material-ui/core/styles";
+import { borders } from '@mui/system';
+import Box from '@mui/material/Box';
+import { red } from '@mui/material/colors';
 
 const AuctionHouse = () => {
 
-    // const classes = useStyles();
+    const renderNameCell = (params) => {
+        return (
+
+            <div >
+
+                <img src='https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350"' height="10px" width="10px" />
+
+                <label
+                    variant="contained"
+                    color="primary"
+                    size="small"
+                    style={{ marginLeft: 16 }}>
+                    {params.value}
+                </label>
+            </div>
+        )
+    }
+
+    const renderCells = (params) => {
+
+        return (
+
+            <Box sx={{}}>
+                <label
+                    variant="contained"
+                    color="primary"
+                    size="small"
+                    style={{}}>
+                    {params.value}
+                </label>
+            </Box>
+        )
+    }
 
     const columns = [
-        { field: 'id', headerName: 'ID', width: 70 },
-        { field: 'firstName', headerName: 'First name', width: 130 },
-        { field: 'lastName', headerName: 'Last name', width: 130 },
+        {
+            field: 'id', headerName: 'Name',
+            minWidth: 200,
+            flex: 1,
+            renderCell: renderNameCell
+        },
+        {
+            field: 'firstName',
+            headerName: 'Rarity',
+            width: 130,
+            flex: 1,
+            renderCell: renderCells
+
+        },
+        {
+            field: 'lastName',
+            headerName: 'Time left',
+            width: 130,
+            flex: 1,
+        },
         {
             field: 'age',
             headerName: 'Age',
             type: 'number',
             width: 90,
+            flex: 1,
         },
         {
             field: 'fullName',
-            headerName: 'Full name',
+            headerName: 'Seller',
             description: 'This column has a value getter and is not sortable.',
             sortable: false,
             width: 160,
+            flex: 1,
             valueGetter: (params) =>
                 `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+        },
+        {
+            field: 'currentOffer',
+            headerName: 'Current offer',
+            width: 130,
+            flex: 1,
+        },
+        {
+            field: 'sellingPrice',
+            headerName: 'Selling price',
+            width: 130,
+            flex: 1,
         },
     ];
 
@@ -44,81 +109,121 @@ const AuctionHouse = () => {
         console.log(event);
     };
 
-
-    // const useStyles = makeStyles((theme) =>
-    //     createStyles({
-    //         root: {
-    //             '& div[data-rowIndex][role="row"]:nth-of-type(5n-4)': {
-    //                 color: "Red",
-    //                 fontSize: 18,
-    //                 //risky
-    //                 minHeight: "60px !important",
-    //                 height: 60,
-    //                 "& div": {
-    //                     minHeight: "60px !important",
-    //                     height: 60,
-    //                     lineHeight: "59px !important"
-    //                 }
-    //             },
-    //             "& .MuiDataGrid-renderingZone": {
-    //                 "& .MuiDataGrid-row": {
-    //                     "&:nth-child(2n)": { backgroundColor: "rgba(235, 235, 235, .7)" }
-    //                 }
-    //             }
-    //         }
-    //     })
-    // );
-
     return (
-        // <div style={{
-        //     backgroundImage: `url("https://nodeguardians.io/_next/image?url=%2Fassets%2Ftextures%2Fbackground-2.webp&w=1800&q=100")`,
-        //     backgroundRepeat: "no-repeat",
-        //     backgroundSize: "cover",
-        //     height: '100vw',
-        //     width: '100vw'
-        // }}>
-        <div className="labelStyle">
-            Auction House
+        <div style={{ height: '70vh', width: '80%', justifyContent: 'center', alignItems: 'center' }}>
+            <DataGrid
+                rows={rows}
+                columns={columns}
+                // pageSize={5}
+                // rowsPerPageOptions={[5]}
+                onRowClick={handleRowClick}
+                rowHeight={120}
+                hideFooter
+                hideFooterPagination
+                hideFooterSelectedRowCount
+                disableColumnMenu
+                sx={{
 
-            <div style={{ height: 400, width: '100%' }}>
-                <DataGrid
-                    // className={classes.root}
-                    rows={rows}
-                    columns={columns}
-                    // pageSize={5}
-                    // rowsPerPageOptions={[5]}
-                    onRowClick={handleRowClick}
-                    rowHeight={120}
-                    sx={{
-                        // HEADER STYLE
-                        "& .MuiDataGrid-columnHeaders": {
-                            backgroundColor: "rgba(0,0,255,0.6)",
-                            color: "rgba(255,0,0,0.7)",
-                            fontSize: 16
+                    // Datagrid root style
+                    '&.MuiDataGrid-root': {
+                        borderRadius: '10px',
+                        border: "1px solid",
+                        borderColor: "rgb(51, 51, 51)",
+                        "&$tableRowSelected, &$tableRowSelected:hover": {
+                            backgroundColor: "rgb(190, 167, 126)"
                         },
+                    },
 
-                        '& div[data-rowIndex][role="row"]:nth-of-type(n)': {
-                            color: "blue",
-                            fontSize: 18,
-                            //risky sizing code starts here
+                    // Header style
+                    '& .MuiDataGrid-columnHeaders': {
+                        color: "rgb(190, 167, 126)",
+                        fontSize: 16,
+                        paddingLeft: '15px',
+                        border: 0,
+                        borderBottom: 1,
+                        borderColor: "rgb(71, 62, 47)",
+                        backgroundColor: "rgb(29, 28, 26)",
+                    },
+
+                    // Row style 
+                    '& .MuiDataGrid-row': {
+                        backgroundColor: 'rgb(6, 6, 6)',
+                        borderRadius: '15px',
+                        marginTop: '4px',
+                        marginLeft: '10px',
+                        marginRight: '40px',
+                        border: "1px solid",
+                        borderColor: "rgb(51, 51, 51)"
+                    },
+
+                    // Cells border bottom
+                    '& div div div div >.MuiDataGrid-cell': {
+                        borderBottom: 'none',
+                    },
+                    '& .MuiDataGrid-columnSeparator': {
+                        display: 'none',
+                    },
+
+                    // All cell style
+                    '& div[data-rowIndex][role="row"]:nth-of-type(n)': {
+                        color: "rgb(205, 205, 205)",
+                        fontSize: 18,
+                        //risky sizing code starts here
+                        minHeight: "60px !important",
+                        height: 60,
+                        // "&:hover": {
+                        //     backgroundColor: "purple"
+                        // },
+                        "&.Mui-selected": {
+                            backgroundColor: "rgba(29, 28, 26, 0.7)",
+                        },
+                        "& div": {
                             minHeight: "60px !important",
                             height: 60,
-                            "& div": {
-                                minHeight: "60px !important",
-                                height: 60,
-                                lineHeight: "59px !important"
-                            }
-                        },
-                        // "& .MuiDataGrid-virtualScrollerRenderZone": {
-                        //     "& .MuiDataGrid-row": {
-                        //         "&:nth-child(2n)": { backgroundColor: "rgba(235, 235, 235, .7)" }
-                        //     }
-                        // }
-                    }}
-                />
-            </div>
-        </div>
+                            lineHeight: "59px !important"
+                        }
+                    },
 
+                    // Remove selected border on cells
+                    '& .MuiDataGrid-cell:focus': {
+                        outline: ' none'
+                    },
+
+                    // Remove selected border on header cells
+                    "& .MuiDataGrid-columnHeader:focus-within, & .MuiDataGrid-columnHeader:focus":
+                    {
+                        outline: "none !important",
+                    },
+
+
+                    // '& .MuiDataGrid-virtualScrollerContent': {
+                    //     paddingBottom: 6, // to compensate space between rows
+                    //     boxSizing: 'content-box',
+                    // },
+
+                    // Arrow sort icon
+                    '.MuiDataGrid-sortIcon': {
+                        opacity: 'inherit !important',
+                        color: "rgb(190, 167, 126)"
+                    },
+
+                    // Data grid scrollbar
+                    '*::-webkit-scrollbar': {
+                        width: '6px',
+                        height: '6px'
+                    },
+                    '*::-webkit-scrollbar-track': {
+                        'borderRadius': '6px',
+                        'boxShadow': 'rgb(15, 15, 15) 0px 0px 6px inset;'
+                    },
+                    '*::-webkit-scrollbar-thumb': {
+
+                        'borderRadius': '6px',
+                        'boxShadow': 'rgb(146, 146, 146) 0px 0px 6px inset'
+                    },
+                }}
+            />
+        </div>
 
     )
 }
